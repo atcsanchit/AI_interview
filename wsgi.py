772602ac25381f51ad555b6d3b296da58e86ad1b
Sanchit Raj <atcsanchit@gmail.com>
2025-01-23 20:47:09 +0530

    Updated port

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 from app import app
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+port_no = os.getenv("PORT")
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    app.run(host='0.0.0.0', port=port_no, debug=True)
